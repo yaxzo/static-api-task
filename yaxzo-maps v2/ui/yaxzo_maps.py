3c9dc5baca4b0f904bbd1901ac5eef8b569806fc
Igor "yaxzo" Bodnar <100714739+yaxzo@users.noreply.github.com>
2023-02-05 17:56:38 +0300

    some fix

--- yaxzo-maps v2/ui/yaxzo_maps.py
+++ yaxzo-maps v2/ui/yaxzo_maps.py
@@ -54,7 +54,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("Yaxzo.Maps", "Yaxzo.Maps"))
         self.map_label.setText(_translate("MainWindow", "Здесь будет ваша карта"))
         self.info_label.setText(_translate("MainWindow", "Что отобразить на карте?"))
         self.search_button.setText(_translate("MainWindow", "Искать"))
